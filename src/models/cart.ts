c897c07bed517478c1992bd7084e12e9b511f406
Gustavo Guichard <gustavoguichard@gmail.com>
2022-12-15 21:20:32 -0300

    feat: create a cart domain-function, parse it with zod and use it in cart route and components

--- src/models/cart.ts
+++ src/models/cart.ts
@@ -1,3 +1,47 @@
-export async function getCart() {
-  return await fetch('https://www.mocky.io/v2/5b15c4923100004a006f3c07').then((r) => r.json())
-}
+import { makeDomainFunction, pipe } from 'domain-functions'
+import * as z from 'zod'
+
+const imageObjectSchema = z.object({
+  featured: z.boolean(),
+  thumbnail: z.string(),
+  small: z.string(),
+  medium: z.string(),
+  large: z.string(),
+  extraLarge: z.string(),
+  valid: z.boolean(),
+})
+
+const priceSpecificationSchema = z.object({
+  sku: z.string(),
+  price: z.number(),
+  originalPrice: z.number(),
+  maxPrice: z.number(),
+  percent: z.number(),
+  discount: z.number(),
+})
+
+const cartItemSchema = z.object({
+  quantity: z.number(),
+  product: z.object({
+    sku: z.string(),
+    name: z.string(),
+    imageObjects: z.array(imageObjectSchema),
+    priceSpecification: priceSpecificationSchema,
+  }),
+})
+
+const cartSchema = z.object({
+  discount: z.number(),
+  id: z.string(),
+  items: z.array(cartItemSchema),
+  shippingTotal: z.number(),
+  subTotal: z.number(),
+  total: z.number(),
+})
+
+const fetchCart = makeDomainFunction()(() =>
+  fetch('https://www.mocky.io/v2/5b15c4923100004a006f3c07').then((r) => r.json()),
+)
+const normalizeCart = makeDomainFunction(cartSchema)(async (cart) => ({ cart }))
+
+export const getCart = pipe(fetchCart, normalizeCart)
--- src/routes/cart.tsx
+++ src/routes/cart.tsx
@@ -1,30 +1,28 @@
-import { ActionFunctionArgs, json, Link, useLoaderData, useRouteLoaderData } from 'react-router-dom'
+import { ActionFunctionArgs, Link, useLoaderData, useRouteLoaderData } from 'react-router-dom'
 import { Box } from '../components/box'
 import { CartItem } from '../components/cart'
-import type { CartType } from '../types/cart'
 import { getCart } from '../models/cart'
 import { pluralize } from '../utils/pluralize'
+import { inputFromForm, UnpackData } from 'domain-functions'
+import { loaderResponseOrNotFound } from '../utils/responses'
+import { getConfig } from '../models/config'
 
 export async function loader() {
-  const cart = await getCart()
-
-  return json(cart)
+  const result = await getCart()
+  return loaderResponseOrNotFound(result)
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-  const formData = await request.formData()
+  const formData = await inputFromForm(request)
   console.log(formData)
 
   return null
 }
 
-type LoaderData = CartType
-
 export default function Cart() {
-  const cart = useLoaderData() as LoaderData
-  const config = useRouteLoaderData('root')
+  const { cart } = useLoaderData() as UnpackData<typeof getCart>
+  const { config } = useRouteLoaderData('root') as UnpackData<typeof getConfig>
   const cartLength = cart.items.length
-  console.log(config)
 
   return (
     <div style={{ display: 'flex', gap: '16px' }}>
